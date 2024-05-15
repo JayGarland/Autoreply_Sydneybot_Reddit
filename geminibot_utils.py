46b2486d14b6bfad81eaad481a16fe1aab0a5c03
JayGarland <128459480+JayGarland@users.noreply.github.com>
2024-05-14 23:56:04 -0700

    update ai model version

--- geminibot_utils.py
+++ geminibot_utils.py
@@ -410,7 +410,7 @@ def sydney_reply(content, context, sub_user_nickname, bot_statement, bot_nicknam
     try:
         persona, pre_reply = init_prompt_botstatement(sub_user_nickname, bot_nickname)
 
-        model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest", safety_settings=SAFETY_SETTINGS, system_instruction=persona + "\n\n" + context)
+        model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest", safety_settings=SAFETY_SETTINGS, system_instruction=persona + "\n\n" + context)
         gemini_messages = askbyuser(ask_string)
         response = model.generate_content(gemini_messages)
         reply_text = response.text
